50847cac2f4b7979ee65c74dcb9e5b9bca49308d
Patrick <patrick204nqh@gmail.com>
2020-05-17 13:15:32 +0700

    catch event and tigger create & delete user in database

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,19 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
 
 // auth trigger (new user signup)
 exports.newUserSignup = functions.auth.user().onCreate((user) => {
-  console.log('user created', user.email, user.uid);
+  // for background triggers you must return a value/promise
+  return admin.firestore().collection('users').doc(user.uid).set({
+    email: user.email,
+    upvotedOn: [],
+  });
 });
 
 // auth trigger (user deleted)
 exports.userDeleted = functions.auth.user().onDelete((user) => {
-  console.log('user deleted', user.email, user.uid);
+  // for background triggers you must return a value/promise
+  const doc = admin.firestore().collection('users').doc(user.uid);
+  return doc.delete();
 });
