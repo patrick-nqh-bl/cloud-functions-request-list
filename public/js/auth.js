8826e4dac0b1ee65cedee84580260a3ac4339ddc
Patrick <patrick204nqh@gmail.com>
2020-05-17 11:46:54 +0700

    auth register form

--- public/js/auth.js
+++ public/js/auth.js
@@ -1,10 +1,34 @@
+import { firebaseConfig } from 'firebase-functions';
+
 const authSwitchLinks = document.querySelectorAll('.switch');
 const authModals = document.querySelectorAll('.auth .modal');
 const authWrapper = document.querySelector('.auth');
+const registerForm = document.querySelector('.register');
+const loginForm = document.querySelector('.login');
+const signOut = document.querySelector('.sign-out');
 
 // toggle auth modals
 authSwitchLinks.forEach((link) => {
   link.addEventListener('click', () => {
     authModals.forEach((modal) => modal.classList.toggle('active'));
   });
+});
+
+// register form
+registerForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const email = registerForm.email.value;
+  const password = registerForm.password.value;
+
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(email, password)
+    .then((user) => {
+      console.log('registered', user);
+      registerForm.reset();
+    })
+    .catch((err) => {
+      registerForm.querySelector('.error').textContent = err.message;
+    });
 });
