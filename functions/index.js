66b79b074dc7ec17b9393ab23f57144f74626be4
Patrick <patrick204nqh@gmail.com>
2020-05-17 12:51:10 +0700

    auth striggers with onCreate() & onDelete() user

--- functions/index.js
+++ functions/index.js
@@ -1,18 +1,11 @@
 const functions = require('firebase-functions');
 
-// http request 1
-exports.randomNumber = functions.https.onRequest((req, res) => {
-  const number = Math.round(Math.random() * 100);
-  res.send(number.toString());
+// auth trigger (new user signup)
+exports.newUserSignup = functions.auth.user().onCreate((user) => {
+  console.log('user created', user.email, user.uid);
 });
 
-// http request 2
-exports.toTheDojo = functions.https.onRequest((req, res) => {
-  res.redirect('https://google.com');
-});
-
-//http callable function
-exports.sayHello = functions.https.onCall((data, context) => {
-  const name = data.name;
-  return `hello, ${name}`;
+// auth trigger (user deleted)
+exports.userDeleted = functions.auth.user().onDelete((user) => {
+  console.log('user deleted', user.email, user.uid);
 });
